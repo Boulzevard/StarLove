cd64509cce254dd26a93967818a760a7137045f5
HugoCalas <hugo.calas31@gmail.com>
2018-05-17 17:47:19 +0200

    ajout activité Search

--- app/src/main/java/fr/wcs/starlove/MainActivity.java
+++ app/src/main/java/fr/wcs/starlove/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.wcs.starlove;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -21,7 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-
+        Button login = findViewById(R.id.bt_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Search.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
