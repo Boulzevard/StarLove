74b8a9d68cf6a5761563f3179b01a8aaf7895c7c
WOLFF Benjamin <wolfbenjamin31@gmail.com>
2018-05-17 16:36:57 +0200

    commit

--- app/src/main/java/fr/wcs/starlove/MainActivity.java
+++ app/src/main/java/fr/wcs/starlove/MainActivity.java
@@ -5,33 +5,24 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,8 +31,10 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference mRef;
     private FirebaseUser mCurrentUser;
     private StorageReference mStorageRef;
+    private long id;
 
     private Intent mGoToMainActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +43,22 @@ public class MainActivity extends AppCompatActivity {
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mStorageRef = FirebaseStorage.getInstance().getReference();
-        mRef = mFirebaseDatabase.getReference("Users");
+        mRef = mFirebaseDatabase.getReference();
         mAuth = FirebaseAuth.getInstance();
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
         mGoToMainActivity = new Intent(MainActivity.this, MainActivity.class);
+
+        mRef.child("Profils").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                id = dataSnapshot.getChildrenCount() + 2;
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
      //  -------------------------- Connection : ----------------------------------------------------------------------
 
@@ -94,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                                         Toast.makeText(MainActivity.this, "wrong Password", Toast.LENGTH_LONG).show();
                                     }
                                 } else {
-                                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                                    Intent intent = new Intent(MainActivity.this, ListeActivity.class);
                                     startActivity(intent);
                                     finish();
                                 }
@@ -106,30 +111,78 @@ public class MainActivity extends AppCompatActivity {
        //  ---------------------------- Création Compte ----------------------------------------------------------------
 
         final Button boutonRegister = findViewById(R.id.bt_signin);
-        boutonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final TextView tvPseudo = findViewById(R.id.tv_pseudo);
-                final EditText etPseudo = findViewById(R.id.et_pseudo);
-                tvPseudo.setVisibility(View.VISIBLE);
-                etPseudo.setVisibility(View.VISIBLE);
-                boutonRegister.setText("Retour");
-                boutonConnection.setText("Créer compte");
+
+            boutonRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView tvPseudo = findViewById(R.id.tv_pseudo);
+                    final EditText etPseudo = findViewById(R.id.et_pseudo);
+                    Button boutonCreation = findViewById(R.id.bt_creation);
+                    tvPseudo.setVisibility(View.VISIBLE);
+                    etPseudo.setVisibility(View.VISIBLE);
+                    boutonConnection.setVisibility(View.GONE);
+                    boutonCreation.setVisibility(View.VISIBLE);
+
+                    boutonCreation.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            EditText editMail = findViewById(R.id.et_email);
+                            EditText editPass = findViewById(R.id.et_pass);
+                            final String pseudo = etPseudo.getText().toString().trim();
+                            String email = editMail.getText().toString().trim();
+                            String password = editPass.getText().toString().trim();
+
+                            if (TextUtils.isEmpty(email)) {
+                                Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                            if (TextUtils.isEmpty(password)) {
+                                Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                            if (password.length() < 6) {
+                                Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+
+                            //create user
+                            mAuth.createUserWithEmailAndPassword(email, password)
+                                    .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            Toast.makeText(MainActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                            // If sign in fails, display a message to the user. If sign in succeeds
+                                            // the auth state listener will be notified and logic to handle the
+                                            // signed in user can be handled in the listener.
+                                            if (!task.isSuccessful()) {
+                                                Toast.makeText(MainActivity.this, "Authentication failed." + task.getException(),
+                                                        Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                String key = mRef.child("Profils").push().getKey();
+                                                mRef.child("Profils").child(key).child("name").setValue(pseudo);
+                                                mRef.child("Profils").child(key).child("id").setValue(id);
+
+                                                startActivity(new Intent(MainActivity.this, ListeActivity.class));
+                                                finish();
+                                            }
+                                        }
+                                    });
+
+                        }
+                    });
+
+
+
+                }
+            });
 
 
 
 
-                boutonRegister.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        tvPseudo.setVisibility(View.VISIBLE);
-                        etPseudo.setVisibility(View.VISIBLE);
-                        boutonRegister.setText("Signin");
-                        boutonConnection.setText("Login");
-                    }
-                });
-            }
-        });
 
 
     }
