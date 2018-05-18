3b18fd0e5c00419b3a99679b57fb53d063900a21
tiga-tiga <gmtsarr@outlook.com>
2018-05-18 10:48:05 +0200

    position camera, limites maps

--- app/src/main/java/fr/wcs/starlove/MapsActivity.java
+++ app/src/main/java/fr/wcs/starlove/MapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -57,10 +58,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         googleMap.setMapStyle(mapFilter);
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+
+        float minZoomPreference = 15;
+        float maxZoomPreference = 4;
+        //mMap.setMinZoomPreference(minZoomPreference);
+        mMap.setMaxZoomPreference(maxZoomPreference);
+
+        LatLngBounds AUSTRALIA = new LatLngBounds(
+            new LatLng(-44, 113), new LatLng(-10, 154));
+
+        // Add a marker in stars and move the camera
         LatLng stars = new LatLng(65.91342517317852, 97.6865230253797);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(stars));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(stars));
+        LatLng one = new LatLng(76.96362432417277, 39.02587325000002);
+        LatLng two = new LatLng(22.314791439224802, 153.98681075000002);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        //add them to builder
+        builder.include(one);
+        builder.include(two);
+
+        LatLngBounds bounds = builder.build();
+
+        //get width and height to current display screen
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+
+        // 20% padding
+        int padding = (int) (width * 0.20);
+
+        //set latlong bounds
+        mMap.setLatLngBoundsForCameraTarget(bounds);
+
+        //move camera to fill the bound to screen
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(stars, 4));
+
+
+
+        //set zoom to level to current so that you won't be able to zoom out viz. move outside bounds
+        mMap.setMinZoomPreference(mMap.getCameraPosition().zoom);
+
 
         mMyRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -100,7 +140,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                
+
                 Intent intent = new Intent(MapsActivity.this, PlanetActivity.class);
                 intent.putExtra("planet", marker.getTitle());
                 startActivity(intent);
